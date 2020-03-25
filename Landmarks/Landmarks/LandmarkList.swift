5dd06c21791ceb29db569480457eb35998406db2
liakassardjian <liakassardjian@gmail.com>
2020-03-25 15:53:27 -0300

    Criação de uma lista de elementos do tipo LandmarkRow

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Lia Kassardjian on 25/03/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandkmarkRow(landmark: landmarkData[0])
+            LandkmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
