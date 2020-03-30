a1306d355185d5c85efd0ab32e1a259d5240ee93
liakassardjian <liakassardjian@gmail.com>
2020-03-30 13:40:32 -0300

    Determinados parâmetros do hexágono

---
+++ Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Lia Kassardjian on 30/03/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
